c78b553ff5e6af1a3afecbbe350fd254c36fcde2
Ryoma Jodoi <sup0ry0jodoi@gmail.com>
2020-12-13 23:16:05 +0900

    Create Server Streaming, Client Streaming, BiDi Streaming API with gRPC

--- src/main/java/com/github/ryoma0622/grpcdemo/greeting/client/GreetingClient.java
+++ src/main/java/com/github/ryoma0622/grpcdemo/greeting/client/GreetingClient.java
@@ -1,21 +1,42 @@
 package com.github.ryoma0622.grpcdemo.greeting.client;
 
 
-import com.proto.greet.GreetRequest;
-import com.proto.greet.GreetResponse;
-import com.proto.greet.GreetServiceGrpc;
-import com.proto.greet.Greeting;
+import com.proto.greet.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.sql.Time;
+import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class GreetingClient {
+
+    ManagedChannel channel;
+
     public static void main(String[] args) {
         System.out.println("Hello I'm a gRPC client");
 
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
+        GreetingClient main = new GreetingClient();
+        main.run();
+    }
+
+    private void run() {
+        channel = ManagedChannelBuilder.forAddress("localhost", 50051)
                 .usePlaintext()
                 .build();
 
+//        doUnaryCall(channel);
+//        doServerStreamingCall(channel);
+//        doClientStreamingCall(channel);
+        doBiDiStreamingCall(channel);
+
+        System.out.println("Shutting down channel");
+        channel.shutdown();
+    }
+
+    private void doUnaryCall(ManagedChannel channel) {
         // created a greet service client ( blocking - synchronous )
         GreetServiceGrpc.GreetServiceBlockingStub greetClient = GreetServiceGrpc.newBlockingStub(channel);
 
@@ -34,8 +55,129 @@ public class GreetingClient {
         GreetResponse greetResponse =  greetClient.greet(greetRequest);
 
         System.out.println(greetResponse.getResult());
+    }
 
-        System.out.println("Shutting down channel");
-        channel.shutdown();
+    private void doServerStreamingCall(ManagedChannel channel) {
+        // created a greet service client ( blocking - synchronous )
+        GreetServiceGrpc.GreetServiceBlockingStub greetClient = GreetServiceGrpc.newBlockingStub(channel);
+
+
+        // Server Streaming
+        GreetManyTimesRequest greetManyTimesRequest = GreetManyTimesRequest.newBuilder()
+                .setGreeting(Greeting.newBuilder().setFirstName("Ryoma").build())
+                .build();
+
+        greetClient.greetManyTimes(greetManyTimesRequest).forEachRemaining(
+                greetManyTimesResponse -> System.out.println(greetManyTimesResponse.getResult())
+        );
+    }
+
+    private void doClientStreamingCall(ManagedChannel channel) {
+        // create a client (stub)
+        GreetServiceGrpc.GreetServiceStub asyncClient = GreetServiceGrpc.newStub(channel);
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        StreamObserver<LongGreetRequest> requestObserver = asyncClient.longGreet(new StreamObserver<LongGreetResponse>() {
+            @Override
+            public void onNext(LongGreetResponse value) {
+                // we get a response from the server
+                System.out.println("Received a response from the server");
+                System.out.println(value.getResult());
+                // onNext will be called only once
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                // we get an error from the server
+            }
+
+            @Override
+            public void onCompleted() {
+                // the serve is done sending us data
+                System.out.println("Server has completed sending us something");
+                latch.countDown();
+            }
+        });
+
+        // streaming message #1
+        System.out.println("sending message 1");
+        requestObserver.onNext(LongGreetRequest.newBuilder()
+            .setGreeting(Greeting.newBuilder()
+                .setFirstName("Ryoma")
+                .build())
+            .build());
+
+        // streaming message #2
+        System.out.println("sending message 2");
+        requestObserver.onNext(LongGreetRequest.newBuilder()
+                .setGreeting(Greeting.newBuilder()
+                        .setFirstName("Ryoma2")
+                        .build())
+                .build());
+
+        // streaming message #3
+        System.out.println("sending message 3");
+        requestObserver.onNext(LongGreetRequest.newBuilder()
+                .setGreeting(Greeting.newBuilder()
+                        .setFirstName("Ryoma3")
+                        .build())
+                .build());
+
+        // we tell the server that the client is done sending data
+        requestObserver.onCompleted();
+
+        try {
+            latch.await(3L, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void doBiDiStreamingCall(ManagedChannel channel) {
+        // create a client (stub)
+        GreetServiceGrpc.GreetServiceStub asyncClient = GreetServiceGrpc.newStub(channel);
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        StreamObserver<GreetEveryoneRequest> requestObserver = asyncClient.greetEveryone(new StreamObserver<GreetEveryoneResponse>() {
+            @Override
+            public void onNext(GreetEveryoneResponse value) {
+                System.out.println("Response from server:" + value.getResult());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                latch.countDown();
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("Server is done sending data");
+                latch.countDown();
+            }
+        });
+
+        Arrays.asList("Ryoma", "Ryoma2", "Ryoma3", "Ryoma4").forEach(
+                name -> {
+                    System.out.println("Sending: " + name);
+                    requestObserver.onNext(GreetEveryoneRequest.newBuilder()
+                            .setGreeting(Greeting.newBuilder()
+                                    .setFirstName(name))
+                            .build());
+                    try {
+                        Thread.sleep(200L);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+
+        requestObserver.onCompleted();
+        try {
+            latch.await(3, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/github/ryoma0622/grpcdemo/greeting/server/GreetServiceImpl.java
+++ src/main/java/com/github/ryoma0622/grpcdemo/greeting/server/GreetServiceImpl.java
@@ -1,9 +1,6 @@
 package com.github.ryoma0622.grpcdemo.greeting.server;
 
-import com.proto.greet.GreetRequest;
-import com.proto.greet.GreetResponse;
-import com.proto.greet.GreetServiceGrpc;
-import com.proto.greet.Greeting;
+import com.proto.greet.*;
 import io.grpc.stub.StreamObserver;
 
 public class GreetServiceImpl extends GreetServiceGrpc.GreetServiceImplBase {
@@ -25,5 +22,85 @@ public class GreetServiceImpl extends GreetServiceGrpc.GreetServiceImplBase {
 
         // complete the RPC call
         responseObserver.onCompleted();
+    }
+
+    @Override
+    public void greetManyTimes(GreetManyTimesRequest request, StreamObserver<GreetManyTimesResponse> responseObserver) {
+        String firstName = request.getGreeting().getFirstName();
+        try {
+            for (int i = 0; i < 10; i++) {
+                String result = "Hello " + firstName + ", response number: " + i;
+                GreetManyTimesResponse response = GreetManyTimesResponse.newBuilder()
+                        .setResult(result)
+                        .build();
+
+                responseObserver.onNext(response);
+                Thread.sleep(1000L);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            responseObserver.onCompleted();
+        }
+    }
+
+    @Override
+    public StreamObserver<LongGreetRequest> longGreet(StreamObserver<LongGreetResponse> responseObserver) {
+
+        StreamObserver<LongGreetRequest> requestStreamObserver = new StreamObserver<LongGreetRequest>() {
+
+            String result = "";
+
+            @Override
+            public void onNext(LongGreetRequest value) {
+                // client sends a message
+                result += "Hello " + value.getGreeting().getFirstName() + "! ";
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                // client send an error
+            }
+
+            @Override
+            public void onCompleted() {
+                // client is done
+                responseObserver.onNext(
+                        LongGreetResponse.newBuilder()
+                        .setResult(result)
+                        .build()
+                );
+                responseObserver.onCompleted();
+            }
+        };
+
+        return requestStreamObserver;
+    }
+
+    @Override
+    public StreamObserver<GreetEveryoneRequest> greetEveryone(StreamObserver<GreetEveryoneResponse> responseObserver) {
+        StreamObserver<GreetEveryoneRequest> requestObserver = new StreamObserver<GreetEveryoneRequest>() {
+            @Override
+            public void onNext(GreetEveryoneRequest value) {
+                String result = "Hello " + value.getGreeting().getFirstName();
+                GreetEveryoneResponse greetEveryoneResponse = GreetEveryoneResponse.newBuilder()
+                        .setResult(result)
+                        .build();
+
+                responseObserver.onNext(greetEveryoneResponse);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                // do nothing
+            }
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onCompleted();
+            }
+        };
+
+        return requestObserver;
     }
 }
